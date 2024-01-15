47e51492600ab8ec5badbe5a3465b77b67a57e60
MCarnegie <mcarnegie@bayviewglen.ca>
2024-01-15 16:27:05 -0500

    tried copying some of old shooter code

---
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -0,0 +1,150 @@
+// Copyright 2021-2023 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.util.Units;
+import frc.robot.Constants;
+import frc.robot.subsystems.StateMachineSubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class Shooter extends StateMachineSubsystemBase {
+
+  private static Shooter instance;
+
+  public static Shooter getInstance() {
+    if (instance == null) {
+      switch (Constants.currentMode) {
+        case SIM:
+          // Sim robot, instantiate physics sim IO implementations
+          instance = new Shooter(new ShooterIOSim());
+          break;
+
+        default:
+          // Replayed robot, disable IO implementations
+          instance = new Shooter(new ShooterIO() {});
+          break;
+      }
+    }
+
+    return instance;
+  }
+
+  public final State DISABLED, IDLE;
+
+  private final ShooterIO io;
+
+  private final SimpleMotorFeedforward ffModel;
+  // hoodangle at 1 rad because angle of hood at max height is around 60 degrees, turret is 180
+  // degrees turret so 90 seems like it would be ok
+
+  /** Creates a new Flywheel. */
+  private Shooter(ShooterIO io) {
+    super("Shooter");
+    this.io = io;
+
+    // Switch constants based on mode (the physics simulator is treated as a
+    // separate robot with different tuning)
+    switch (Constants.currentMode) {
+      case REAL:
+      case REPLAY:
+        ffModel = new SimpleMotorFeedforward(0.1, 0.05);
+        io.flywheelConfigurePID(1.0, 0.0, 0.0);
+        break;
+      case SIM:
+        ffModel = new SimpleMotorFeedforward(0.0, 0.03);
+        io.flywheelConfigurePID(0.5, 0.0, 0.0);
+        break;
+      default:
+        ffModel = new SimpleMotorFeedforward(0.0, 0.0);
+        break;
+    }
+
+    DISABLED =
+        new State("DISABLED") {
+
+          @Override
+          public void init() {
+            stop();
+            // stop hood
+            // stop target angle spining
+          }
+
+          @Override
+          public void periodic() {}
+
+          @Override
+          public void exit() {}
+        };
+
+    IDLE =
+        new State("IDLE") {
+
+          @Override
+          public void init() {
+            stop();
+          }
+
+          @Override
+          public void periodic() {}
+
+          @Override
+          public void exit() {}
+        };
+
+   
+  }
+
+  @Override
+  public void inputPeriodic() {
+    
+  }
+
+  @Override
+  public void outputPeriodic() {
+
+    // Log flywheel speed in RPM
+    Logger.recordOutput("FlywheelSpeedRPM", getVelocityRPM());
+  }
+
+  /** Run closed loop at the specified velocity. */
+  public void runFlywheelAtVelocity(double velocityRPM) {
+    var velocityRadPerSec = Units.rotationsPerMinuteToRadiansPerSecond(velocityRPM);
+    io.setFlywheelVelocity(velocityRadPerSec, ffModel.calculate(velocityRadPerSec));
+
+    // Log flywheel setpoint
+    Logger.recordOutput("FlywheelSetpointRPM", velocityRPM);
+  }
+
+  /** Stops the flywheel. */
+  public void stop() {
+    io.stop();
+  }
+
+  /** Returns the current velocity in RPM. */
+  public double getVelocityRPM() {
+    return 0.0;
+    //will return something different when we get the inputs
+
+   // return Units.radiansPerSecondToRotationsPerMinute(inputs.flywheelVelocityRadPerSec);
+  }
+
+  /** Runs forwards at the commanded voltage. */
+  public void runCharacterizationVolts(double volts) {
+    io.setFlywheelVoltage(volts);
+  }
+
+
+  
+}
---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems.shooter;
+
+public interface ShooterIO {
+
+    public static class ShooterIOInputs{
+        public double flywheelVelocityRadPerSec = 0.0;
+        public double flywheelAppliedVolts = 0.0;
+        public double hoodPositionRad = 0.0;
+        public double hoodAppliedVolts = 0.0;
+        public double hoodVelocityRadPerSec = 0.0;
+        public double turretVelocityRadPerSec = 0.0;
+        public double turretAppliedVolts = 0.0;
+        public double turretPositionRad = 0.0;
+        public double[] currentAmps = new double[] {};
+
+    }
+            /** Updates the set of loggable inputs. */
+        public default void updateInputs(ShooterIOInputs inputs) {}
+
+        /** Run open loop at the specified voltage. */
+        public default void setFlywheelVoltage(double volts) {}
+
+        /** Run closed loop at the specified velocity. */
+        public default void setFlywheelVelocity(double velocityRadPerSec, double ffVolts) {}
+
+        /** Stop in open loop. */
+        public default void stop() {}
+
+        /** Set velocity PID constants. */
+        public default void flywheelConfigurePID(double kP, double kI, double kD) {}
+
+        public default void turretConfigurePID(double kP, double kI, double kD) {}
+
+        public default void hoodConfigurePID(double kP, double kI, double kD) {}
+
+        public default void setTurretAngle() {}
+} 
