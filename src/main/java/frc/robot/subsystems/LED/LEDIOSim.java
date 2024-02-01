659db71c1048c1f264d60876d25252c625e314d0
Joline Lian <jlian2@bayviewglen.ca>
2024-02-01 15:38:09 -0500

    LED updates

--- src/main/java/frc/robot/subsystems/LED/LEDIOSim.java
+++ src/main/java/frc/robot/subsystems/LED/LEDIOSim.java
@@ -1,27 +1,26 @@
 package frc.robot.subsystems.LED;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.simulation.AddressableLEDSim;
 
-public class LEDIOSim implements LEDIO{
-    private AddressableLED m_led = new AddressableLED(14);
-    private AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(36);
+public class LEDIOSim implements LEDIO {
+  public AddressableLEDSim m_led = new AddressableLEDSim();
+  public byte[] m_ledBuffer = new byte[36 * 3];
 
-    public LEDIOSim() {
-        m_led.setLength(m_ledBuffer.getLength());
-        m_led.setData(m_ledBuffer);
-        m_led.start();
-    }
-
-    @Override
-    public void updateInputs(LEDIOInputs inputs) {}
-
-    @Override
-    public void setColours(boolean isRGB, int[] leds) {
-        for (int i=0; i<m_ledBuffer.getLength(); i++) {
-            if (isRGB) m_ledBuffer.setHSV(i, leds[i*3 + 0], leds[i*3 + 1], leds[i*3 + 2]);
-            else m_ledBuffer.setRGB(i, leds[i*3 + 0], leds[i*3 + 1], leds[i*3 + 2]);
-        }
+  public LEDIOSim() {
+    AddressableLEDSim.createForChannel(14);
+    m_led.setLength(36 * 3);
     m_led.setData(m_ledBuffer);
+    // m_led.start();
+  }
+
+  @Override
+  public void updateInputs(LEDIOInputs inputs) {}
+
+  @Override
+  public void setColours(boolean isRGB, int[] leds) {
+    for (int i = 0; i < leds.length; i++) {
+        m_ledBuffer[i] = (byte) leds[i];
     }
+    m_led.setData(m_ledBuffer);
+  }
 }
