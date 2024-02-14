e881eb7243bf36d94c89e422504d9ba78d176950
Cameron <cwoodard@bayviewglen.ca>
2024-02-13 19:39:06 -0500

    Intaking logic works

--- src/main/java/frc/robot/commands/Temp.java
+++ src/main/java/frc/robot/commands/Temp.java
@@ -4,25 +4,23 @@
 
 package frc.robot.commands;
 
-import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.OI;
 import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.intake.Intake;
 
 public class Temp extends Command {
 
   private final Drive drive;
-  private TalonFX turret;
-  private DutyCycleOut out;
+  private final Intake intake;
+
+  boolean hasGamePiece = false;
 
   /** Creates a new DriveTeleop. */
   public Temp() {
     // Use addRequirements() here to declare subsystem dependencies.
     drive = Drive.getInstance();
-
-    turret = new TalonFX(40);
-    out = new DutyCycleOut(0);
+    intake = Intake.getInstance();
 
     addRequirements(drive);
   }
@@ -31,34 +29,55 @@ public class Temp extends Command {
   @Override
   public void initialize() {
     drive.setCurrentState(drive.DISABLED);
+    intake.setCurrentState(intake.IDLE);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
 
+    if (!intake.isState(intake.DISABLED)) {
+
+      if (intake.beamBroken()) {
+        hasGamePiece = true;
+      } else if (OI.DR.getRightTriggerAxis() > 0) {
+        hasGamePiece = false;
+      }
+
+      if (OI.DR.getAButton()) {
+        if (hasGamePiece && !intake.beamBroken()) {
+          intake.setCurrentState(intake.IDLE);
+        } else if (!hasGamePiece) {
+          intake.setCurrentState(intake.INTAKING);
+        }
+      } else if (OI.DR.getBButton()) {
+        intake.setCurrentState(intake.AMP_SIDE_2);
+      } else if (OI.DR.getXButton()) {
+        intake.setCurrentState(intake.SHOOTER_SIDE);
+      } else {
+        intake.setCurrentState(intake.IDLE);
+      }
+    }
+
     if (!drive.isState(drive.DISABLED)) {
       // slow mode
       // x stance while shooting
-      if (OI.DR.getLeftTriggerAxis() > 0) {
-        drive.setCurrentState(drive.SHOOTING);
-      } else
-        // autolocking
-        // if (OI.DR.getXButton()) {
-        //   drive.setAutolockSetpoint(-61.19);
-        //   drive.setCurrentState(drive.STRAFE_AUTOLOCK);
-        // } else if (OI.DR.getAButton()) {
-        //   drive.setAutolockSetpoint(0);
-        //   drive.setCurrentState(drive.STRAFE_AUTOLOCK);
-        // } else if (OI.DR.getBButton()) {
-        //   drive.setAutolockSetpoint(59.04);
-        //   drive.setCurrentState(drive.STRAFE_AUTOLOCK);
-        // } else if (OI.DR.getYButton()) {
-        //   drive.setAutolockSetpoint(90);
-        //   drive.setCurrentState(drive.STRAFE_AUTOLOCK);
-        // } else {
-        // strafe and turn if not other state
-        drive.setCurrentState(drive.STRAFE_N_TURN);
+      // autolocking
+      // if (OI.DR.getXButton()) {
+      //   drive.setAutolockSetpoint(-61.19);
+      //   drive.setCurrentState(drive.STRAFE_AUTOLOCK);
+      // } else if (OI.DR.getAButton()) {
+      //   drive.setAutolockSetpoint(0);
+      //   drive.setCurrentState(drive.STRAFE_AUTOLOCK);
+      // } else if (OI.DR.getBButton()) {
+      //   drive.setAutolockSetpoint(59.04);
+      //   drive.setCurrentState(drive.STRAFE_AUTOLOCK);
+      // } else if (OI.DR.getYButton()) {
+      //   drive.setAutolockSetpoint(90);
+      //   drive.setCurrentState(drive.STRAFE_AUTOLOCK);
+      // } else {
+      // strafe and turn if not other state
+      // drive.setCurrentState(drive.STRAFE_N_TURN);
       // }
 
       // if (OI.XK.get(0, 0)) {
@@ -67,16 +86,6 @@ public class Temp extends Command {
       //   drive.setModuleModes(Mode.SETPOINT);
       // }
     }
-
-    if (OI.DR.getAButton()) {
-      out = new DutyCycleOut(0.10);
-    } else if (OI.DR.getBButton()) {
-      out = new DutyCycleOut(-0.1);
-    } else {
-      out = new DutyCycleOut(0);
-    }
-
-    turret.setControl(out);
   }
 
   // Called once the command ends or is interrupted.
