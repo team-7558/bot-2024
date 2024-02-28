b8b327db73d3254617bf3dc5df3c729e0463c2e6
Cameron <cwoodard@bayviewglen.ca>
2024-02-28 18:29:00 -0500

    changed current path to calibration

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.auto.Calibration;
 import frc.robot.auto.RunAltAutoCommand;
 import frc.robot.auto.Test;
 import frc.robot.commands.RobotTeleop;
@@ -87,7 +88,7 @@ public class RobotContainer {
     //         drive // Reference to this subsystem to set requirements
     // );
 
-    return new RunAltAutoCommand(new Test());
+    return new RunAltAutoCommand(new Calibration());
 
     /*PathPlannerPath path = PathPlannerPath.fromChoreoTrajectory("4note");
     List<PathPoint> points = path.getAllPathPoints();
