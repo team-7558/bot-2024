330549d3458a0f32c3d8a9d7a51c2bcdcf871d39
Cameron <cwoodard@bayviewglen.ca>
2024-02-28 18:22:24 -0500

    paths and calibration auto - ethan

---
+++ src/main/java/frc/robot/auto/Calibration.java
@@ -0,0 +1,23 @@
+package frc.robot.auto;
+
+public class Calibration extends AltAuto  {
+
+    public Calibration() {
+        super("Calibration");
+
+        trajstack.appendChain().append("Calibration Real", false);
+    
+        trajstack.setActiveIdx(0);
+        trajstack.generate();
+    }
+
+    @Override
+    public void onInit() {
+
+    }
+
+    @Override
+    public void onExecute() {
+    }
+    
+}
