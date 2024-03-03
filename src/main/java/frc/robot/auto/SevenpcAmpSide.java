3237508698948a50434337ced17fed230a1de43f
Cameron <cwoodard@bayviewglen.ca>
2024-03-02 19:07:52 -0500

    SevenpcAmpSide

--- src/main/java/frc/robot/auto/SevenpcAmpSide.java
+++ src/main/java/frc/robot/auto/SevenpcAmpSide.java
@@ -17,8 +17,32 @@ public class SevenpcAmpSide extends AltAuto {
   @Override
   public void onExecute() {
 
-    if(between(1,2)) {
-      ss.feedFromGroundAutoPreset();
-    }
+    // if(between(1,1.30)) {
+    //   ss.shoot();
+    // }
+    // if(between(2.85, 3.15)) {
+    //   ss.feedFromGroundAutoPreset();
+    // }
+    // if(between(4,4.5)) {
+    //   ss.shoot();
+    // }
+    // if(between(5.5, 5.8)) {
+    //   ss.feedFromGroundAutoPreset();
+    // }
+    // if(between(6.75,7.05)) {
+    //   ss.shoot();
+    // }
+    // if(between(8.3, 8.6)) {
+    //   ss.feedFromGroundAutoPreset();
+    // }
+    // if(between(11.5,11.8)) {
+    //   ss.shoot();
+    // }
+    // if(between(12.5, 15.7  )) {
+    //   ss.feedFromGroundAutoPreset();
+    // }
+    // if(between(12.5, 15.7)) {
+    //   ss.feedFromGroundAutoPreset();
+    // }
   }
 }
